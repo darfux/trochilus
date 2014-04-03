0f8c8334a021eb554bab2f9d9e67f2e3e009ecb5
darfux <darfux@163.com>
2014-04-03 18:27:00 +0800

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
-  validates_presence_of :password, :password_confirmation
+  validates_presence_of :password, :password_confirmation # validate confirmation will not work if password_fonfirmation is nil! 
+                                                          # Don't forget to check your patams permit in controller too.
   validates_confirmation_of :password
   validates :account, :presence => true, :uniqueness => true
   validates :password, :presence => true, :on => :create
