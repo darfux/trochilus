13bbfa2bfb7c409f70e399c194c7fefc1d8ff8c7
darfux <darfux@163.com>
2014-07-11 22:56:48 +0800

    add devise:user

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :account, uniqueness: true, presence: true
+  protected
+    def email_required?
+      false
+    end
+end
