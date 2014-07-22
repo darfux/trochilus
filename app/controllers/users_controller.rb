19f71a0bd937632beae59d42f19e655464065932
darfux <darfux@163.com>
2014-07-22 23:55:10 +0800

    working on user search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def search
+    search_name = params[:search]
+    # raise params[:search].inspect
+    # project = Project.find(params[:project_id])
+    unless search_name
+      @customers = IndividualCustomer.all
+    else
+      @customers = Customer.where(
+        "name LIKE ? AND customer_type = ?", "%#{search_name}%", IndividualCustomer.to_s).collect{ |c| c.customer }
+    end
+    respond_to do |format|
+      format.js
+    end
   end
 end
