756dcea533289ef340f834bb68032fb778cc6336
darfux <darfux@163.com>
2014-07-28 13:26:59 +0800

    Add ruby-pinyin. Improve UnivUnit.

--- app/models/univ_unit.rb
+++ app/models/univ_unit.rb
@@ -2,4 +2,12 @@ class UnivUnit < ActiveRecord::Base
   has_many :majors
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  def self.all_spy
+    all.sort_by{ |e| e.name_with_py }
+  end
+  
+  def name_with_py
+    PinYin.abbr(name)[0].upcase+'-'+name
+  end
 end
