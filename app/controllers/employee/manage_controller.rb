e1ba31d6585c26143f24f451ee7e90852aa1354a
darfux <darfux@163.com>
2014-07-28 18:22:49 +0800

    advanced fund list show --add user filter

--- app/controllers/employee/manage_controller.rb
+++ app/controllers/employee/manage_controller.rb
@@ -11,7 +11,16 @@ class Employee::ManageController < ApplicationController
   end
 
   def funds
-    @actual_funds = Fund.where('fund_instance_type != ?', 'DonationRecord').order('updated_at DESC')
-    @plan_funds = Fund.where('fund_instance_type == ?', 'DonationRecord').order('updated_at DESC')
+    @actual_funds = (
+      draf = Fund.joins("JOIN donation_record_actual_funds ON donation_record_actual_funds.id = funds.fund_instance_id 
+        AND funds.fund_instance_type = 'DonationRecord::ActualFund'").joins("JOIN donation_records ON 
+        donation_record_actual_funds.donation_record_id = donation_records.id").where("donation_records.creator_id = ? ", current_user.id)
+      uf = Fund.joins("JOIN usage_record_used_funds ON usage_record_used_funds.id = funds.fund_instance_id 
+        AND funds.fund_instance_type = 'UsageRecord::UsedFund'").joins("JOIN usage_records ON 
+        usage_record_used_funds.usage_record_id = usage_records.id").where("usage_records.creator_id = ? ", current_user.id)
+      (draf+uf).sort_by{ |e| e.created_at }.reverse!
+    )
+    @plan_funds = Fund.joins("JOIN donation_records ON donation_records.id = funds.fund_instance_id 
+      AND funds.fund_instance_type = 'DonationRecord'").where("donation_records.creator_id = ? ", current_user.id).to_a.reverse!
   end
 end
