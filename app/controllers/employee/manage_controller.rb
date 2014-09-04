a7bfb9e0988a78623d275009ad074fa42f3dafe1
darfux <darfux@163.com>
2014-09-04 21:41:57 +0800

    add basic filter and sort for customer and fund cuatomer:   name fund:   project, amount, time

--- app/controllers/employee/manage_controller.rb
+++ app/controllers/employee/manage_controller.rb
@@ -16,30 +16,34 @@ class Employee::ManageController < ApplicationController
   end
 
   def customers
-    @customers = (
+     tmp = (
       if current_user.account.to_s == 'fkadmin'
-        Customer.all.reverse
+        Customer.all
       else
-        current_people.created_customers!.reverse
+        current_people.created_customers!
       end
     )
+    tmp = handle_filter(tmp)
+    @customers = tmp
   end
 
   def funds
-    @actual_funds = (
+    tmp = (
       if current_user.account.to_s == 'fkadmin'
-        Fund.where('fund_instance_type != ?', 'DonationRecord').order('updated_at DESC')
+        Fund.where('fund_instance_type != ?', 'DonationRecord').order('time DESC')
       else
-        get_actual_funds
+        get_actual_funds(((f=params[:filters]) ? f[:fund_direction] : nil))
       end
     )
-    @plan_funds = (
+    @actual_funds = handle_sort(tmp)
+    tmp = (
       if current_user.account.to_s == 'fkadmin'
-        Fund.where('fund_instance_type == ?', 'DonationRecord').order('updated_at DESC')
+        Fund.where('fund_instance_type == ?', 'DonationRecord').order('time DESC')
       else
-        get_plan_funds
+        get_plan_funds.order('time DESC')
       end
     ) 
+    @plan_funds = handle_sort(tmp)
   end
 
   def others
@@ -65,17 +69,24 @@ class Employee::ManageController < ApplicationController
       end
       relation.where(f)
     end
-    def get_actual_funds
-      draf = Fund.joins("JOIN donation_record_actual_funds ON donation_record_actual_funds.id = funds.fund_instance_id 
-        AND funds.fund_instance_type = 'DonationRecord::ActualFund'").joins("JOIN donation_records ON 
-        donation_record_actual_funds.donation_record_id = donation_records.id").where("donation_records.creator_id = ? ", current_user.id)
-      uf = Fund.joins("JOIN usage_record_used_funds ON usage_record_used_funds.id = funds.fund_instance_id 
-        AND funds.fund_instance_type = 'UsageRecord::UsedFund'").joins("JOIN usage_records ON 
-        usage_record_used_funds.usage_record_id = usage_records.id").where("usage_records.creator_id = ? ", current_user.id)
-      (draf+uf).sort_by{ |e| e.created_at }.reverse!
+    def get_actual_funds(direction='all')
+      af = []
+      if direction!='out'
+        draf = Fund.joins("JOIN donation_record_actual_funds ON donation_record_actual_funds.id = funds.fund_instance_id 
+          AND funds.fund_instance_type = 'DonationRecord::ActualFund'").joins("JOIN donation_records ON 
+          donation_record_actual_funds.donation_record_id = donation_records.id").where("donation_records.creator_id = ? ", current_user.id)
+        af.concat draf
+      end
+      if direction!='in'
+        uf = Fund.joins("JOIN usage_record_used_funds ON usage_record_used_funds.id = funds.fund_instance_id 
+          AND funds.fund_instance_type = 'UsageRecord::UsedFund'").joins("JOIN usage_records ON 
+          usage_record_used_funds.usage_record_id = usage_records.id").where("usage_records.creator_id = ? ", current_user.id)
+        af.concat uf
+      end
+      af.sort_by{ |e| e.time }.reverse!
     end
     def get_plan_funds
       Fund.joins("JOIN donation_records ON donation_records.id = funds.fund_instance_id 
-        AND funds.fund_instance_type = 'DonationRecord'").where("donation_records.creator_id = ? ", current_user.id).to_a.reverse!
+        AND funds.fund_instance_type = 'DonationRecord'").where("donation_records.creator_id = ? ", current_user.id)
     end
 end
