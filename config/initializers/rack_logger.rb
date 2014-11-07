66f819bcf03352662bcf6b58c728b5e2cd58ced4
darfux <darfux@163.com>
2014-11-07 12:05:01 +0800

    change rack logger only log HTTP header under production

--- config/initializers/rack_logger.rb
+++ config/initializers/rack_logger.rb
@@ -1,11 +1,13 @@
-module Rails
-  module Rack
-    # Sets log tags, logs the request, calls the app, and flushes the logs.
-    class Logger < ActiveSupport::LogSubscriber
-      alias_method :org_started_request_message, :started_request_message 
-      def started_request_message(request)
-        l = org_started_request_message(request)
-        l + %Q{ -ref-"#{request.referer}" -agent-"#{request.env["HTTP_USER_AGENT"]}" -accept-"#{request.env["HTTP_ACCEPT"]}"}
+if Rails.env.production?
+  module Rails
+    module Rack
+      # Sets log tags, logs the request, calls the app, and flushes the logs.
+      class Logger < ActiveSupport::LogSubscriber
+        alias_method :org_started_request_message, :started_request_message 
+        def started_request_message(request)
+          l = org_started_request_message(request)
+          l + %Q{ -ref-"#{request.referer}" -agent-"#{request.env["HTTP_USER_AGENT"]}" -accept-"#{request.env["HTTP_ACCEPT"]}"}
+        end
       end
     end
   end
